355276c5b029ab4c69415dcabdf2c3d612125fb2
hrishabhp <hrishabh.pal@gmail.com>
2025-11-05 17:15:07 +0530

    Initial commit

---
+++ Basic Operation.py
@@ -0,0 +1,19 @@
+num1 = 10
+num2 = 20
+
+#addition
+S = int(num1 + num2)
+# S= sum of num1 and num2
+print('The sum is',S)
+
+#subtraction
+S = int(num1 - num2)
+print('The difference is',S)
+
+#multiplication
+S = int(num1 * num2)
+print('The product is',S)
+
+#division
+S = float(num1/num2)
+print('The division is',S)
